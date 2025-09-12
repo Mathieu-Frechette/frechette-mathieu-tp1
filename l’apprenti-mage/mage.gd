c0039facb2876fb2dff2942f2cc11fd85beca186
Mathieu-Frechette <mathieu.frechette02@gmail.com>
2025-09-11 21:33:42 -0400

    Animation

---
+++ l’apprenti-mage/mage.gd
@@ -0,0 +1,20 @@
+extends AnimatedSprite2D
+
+@onready var attack1_sound = AudioStreamPlayer2D.new()
+
+func _ready():
+	add_child(attack1_sound)
+	attack1_sound.stream = load("res://assets/music/magic-spell-333896.mp3")
+	
+func _ready():
+	play("default")
+	animation_finished.connect(_on_animation_finished)
+
+func _process(_delta):
+	if Input.is_key_pressed(KEY_W):
+		if animation != "Attack1":   # Vérifie l'animation en cours
+			play("Attack1")
+
+func _on_animation_finished():
+	if animation == "Attack1":       # Quand Attack1 se termine, on revient à "default"
+		play("default")
